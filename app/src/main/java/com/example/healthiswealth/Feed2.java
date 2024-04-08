127f76d2bda43876c8199417a6bd1f5144372c04
Abhay Khator <abhaykhator69@gmail.com>
2024-04-08 14:39:39 +0530

    added feed in feed1 and feed2

--- app/src/main/java/com/example/healthiswealth/Feed2.java
+++ app/src/main/java/com/example/healthiswealth/Feed2.java
@@ -1,22 +1,48 @@
 package com.example.healthiswealth;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Feed2 extends AppCompatActivity {
     Button logout;
     Button home;
+    ListView listView;
+    List<String> headlines;
+    List<String> links;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed2);
+
+        listView = findViewById(R.id.listView);
+        headlines = new ArrayList<>();
+        links = new ArrayList<>();
+
+        new FetchFeedTask().execute();
 
         logout = findViewById(R.id.Logout_btn);
         home = findViewById(R.id.Btn_home);
@@ -33,11 +59,71 @@ public class Feed2 extends AppCompatActivity {
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(getApplicationContext(), com.example.healthiswealth.MainActivity.class);
+                Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent2);
                 finish();
             }
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Uri uri = Uri.parse(links.get(position));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class FetchFeedTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                URL url = new URL("https://example.tumblr.com/rss");
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput(getInputStream(url), "UTF-8");
+                boolean insideItem = false;
+
+                int eventType = xpp.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG) {
+                        if (xpp.getName().equalsIgnoreCase("item")) {
+                            insideItem = true;
+                        } else if (xpp.getName().equalsIgnoreCase("title")) {
+                            if (insideItem)
+                                headlines.add(xpp.nextText());
+                        } else if (xpp.getName().equalsIgnoreCase("link")) {
+                            if (insideItem)
+                                links.add(xpp.nextText());
+                        }
+                    } else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")) {
+                        insideItem = false;
+                    }
+                    eventType = xpp.next();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void result) {
+            adapter = new ArrayAdapter<>(Feed2.this, android.R.layout.simple_list_item_1, headlines);
+            listView.setAdapter(adapter);
+        }
+    }
+
+    public InputStream getInputStream(URL url) {
+        try {
+            return url.openConnection().getInputStream();
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/healthiswealth/rssFeed.java
+++ app/src/main/java/com/example/healthiswealth/rssFeed.java
@@ -4,11 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
 
 public class rssFeed extends AppCompatActivity {
 
@@ -16,6 +15,7 @@ public class rssFeed extends AppCompatActivity {
     Button home;
 
     Button feed1,feed2;
+    ArrayList<String> rssLinks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,9 @@ public class rssFeed extends AppCompatActivity {
         home = findViewById(R.id.Btn_home);
         feed1 = findViewById(R.id.feed1);
         feed2 = findViewById(R.id.feed2);
+
+        rssLinks.add("http://www.rediff.com/rss/moviesreviewsrss.xml");
+        rssLinks.add("http://www.cinemablend.com/rss_review.php");
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +52,7 @@ public class rssFeed extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent2 = new Intent(getApplicationContext(), com.example.healthiswealth.Feed1.class);
+                intent2.putExtra("rssLink", rssLinks.get(0));
                 startActivity(intent2);
                 finish();
             }
@@ -58,6 +62,7 @@ public class rssFeed extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent2 = new Intent(getApplicationContext(), com.example.healthiswealth.Feed2.class);
+                intent2.putExtra("rssLink", rssLinks.get(1));
                 startActivity(intent2);
                 finish();
             }
@@ -65,4 +70,5 @@ public class rssFeed extends AppCompatActivity {
 
 
     }
+
 }
